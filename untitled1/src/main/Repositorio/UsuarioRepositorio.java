fdcfef81611abd23158af0b792e7c2a2daa16fee
jonibog <135062183+jonibog@users.noreply.github.com>
2024-12-13 18:51:26 -0300

    Add files via upload

---
+++ untitled1/src/main/Repositorio/UsuarioRepositorio.java
@@ -0,0 +1,7 @@
+package main.Repositorio;
+
+import main.modelo.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
+}
